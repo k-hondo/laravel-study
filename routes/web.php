0a242ce193f5d704a79a0a7e7541545553aeda79
k-hondo <hdkk.7.4.1.9.5.3@gmail.com>
2026-03-25 18:10:01 +0900

    add: /hello-world ルートをHTML形式で返すように変更

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello-world', fn() => 'hello world');
+Route::get('/hello-world', fn() => '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World!</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+</body>
+</html>');
