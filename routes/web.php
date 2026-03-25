7fc833168c5f92488f97370f08334b2024ca6f87
k-hondo <hdkk.7.4.1.9.5.3@gmail.com>
2026-03-25 18:14:30 +0900

    add: /hello-world ルートをビューを使用して返すように変更し、hello_world.blade.phpを追加

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello-world', fn() => '<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello World!</title>
-</head>
-<body>
-    <h1>Hello World!</h1>
-</body>
-</html>');
+Route::get('/hello-world', fn() => view('hello_world'));
