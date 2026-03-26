a05062a977dfe5cd529b22a29b3d40b70a2e99d0
k-hondo <hdkk.7.4.1.9.5.3@gmail.com>
2026-03-26 15:56:44 +0900

    fix: トップページとカリキュラムページのルートを修正

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/hello-world', fn() => view('hello_world'));
 Route::get('/hello', fn() => view('hello', [
     'name' => '山田',
     'course' => 'Laravel'
 ]));
+
+// トップページ
+Route::get('/', fn() => view('index'));
+
+// カリキュラムページ
+Route::get('/curriculum', fn() => view('curriculum'));
