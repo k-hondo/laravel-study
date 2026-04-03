7196efd806d59b6cc439212da37df75fc6b25628
k-hondo <hdkk.7.4.1.9.5.3@gmail.com>
2026-04-03 18:36:49 +0900

    refactor: モデルにリレーション追加

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    /**
+     * ブログとのリレーション
+     */
+    public function blogs()
+    {
+        return $this->hasMany(Blog::class);
+    }
 }
