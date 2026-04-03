c0d3d42e6fae6ae65093296a47998b43d0f43ad8
k-hondo <hdkk.7.4.1.9.5.3@gmail.com>
2026-04-03 14:40:24 +0900

    add: ブログ更新処理追加

--- app/Http/Controllers/Admin/AdminBlogController.php
+++ app/Http/Controllers/Admin/AdminBlogController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Http\Requests\Admin\StoreBlogRequest;
+use App\Http\Requests\Admin\UpdateBlogRequest;
 use App\Models\Blog;
+use Illuminate\Support\Facades\Storage;
 
 class AdminBlogController extends Controller
 {
@@ -64,11 +65,25 @@ class AdminBlogController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
+     * 指定したIDのブログ更新処理
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateBlogRequest $request, string $id)
     {
-        //
+        $blog = Blog::findOrFail($id);
+        $updateData = $request->validated();
+
+        // 画像を変更する場合
+        if ($request->has('image')) {
+            // 変更前の画像を削除
+            Storage::disk('public')->delete($blog->image);
+            // 変更後の画像をアップロード、保存パスを更新対象データにセット
+            $updateData['image'] = $request->file('image')->store('blogs', 'public');
+        }
+        // ブログの更新
+        $blog->update($updateData);
+
+        // ブログ一覧画面へリダイレクト
+        return to_route('admin.blogs.index')->with('success', 'ブログを更新しました');
     }
 
     /**
