d2c0d945a57488d4c924f7b6a1d492afde74c3d5
k-hondo <hdkk.7.4.1.9.5.3@gmail.com>
2026-04-03 19:58:47 +0900

    feat: ログイン機能追加

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\PhotoController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\Admin\AdminBlogController;
 use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\AuthController;
 
 
 // ////////////////////////////////////////////////
@@ -35,6 +36,10 @@ Route::delete('/admin/blogs/{blog}', [AdminBlogController::class, 'destroy'])->n
 // ユーザ管理
 Route::get('/admin/users/create', [UserController::class, 'create'])->name('admin.users.create');
 Route::post('/admin/users', [UserController::class, 'store'])->name('admin.users.store');
+
+// 認証
+Route::get('/admin/login', [AuthController::class, 'showLoginForm'])->name('admin.login');
+Route::post('/admin/login', [AuthController::class, 'login']);
 
 
 
