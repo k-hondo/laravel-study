0165d4c866076313bd66552c201f3602ffbf3b90
k-hondo <hdkk.7.4.1.9.5.3@gmail.com>
2026-04-09 11:30:43 +0900

    feat: レイアウトのリンク追加

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -26,14 +26,16 @@
                             href="{{ route('cats.index') }}">ねこちゃんたち</a></li>
                     <li><a class="text-sm {{ request()->routeIs('blogs.index') ? 'text-blue-500 font-bold' : 'text-blueGray-400' }} hover:text-blueGray-500"
                             href="{{ route('blogs.index') }}">ブログ</a></li>
-                    <li><a class="text-sm text-blueGray-400 hover:text-blueGray-500" href="#">メニュー</a></li>
-                    <li><a class="text-sm text-blueGray-400 hover:text-blueGray-500" href="#">よくあるご質問</a></li>
+                    <li><a class="text-sm {{ request()->routeIs('menu') ? 'text-blue-500 font-bold' : 'text-blueGray-400' }} hover:text-blueGray-500"
+                            href="{{ route('menu') }}">メニュー</a></li>
+                    <li><a class="text-sm {{ request()->routeIs('faq') ? 'text-blue-500 font-bold' : 'text-blueGray-400' }} hover:text-blueGray-500"
+                            href="{{ route('faq') }}">よくあるご質問</a></li>
                 </ul>
                 <div>
                     <a class="mr-2 inline-block px-4 py-3 text-xs text-blue-500 hover:text-blue-600 leading-none border border-blue-200 hover:border-blue-300 rounded"
-                        href="/contact">お問い合わせ</a>
+                        href="{{ route('contact') }}">お問い合わせ</a>
                     <a class="inline-block px-4 py-3 text-xs font-semibold leading-none bg-blue-500 hover:bg-blue-600 text-white rounded"
-                        href="/#access">アクセス</a>
+                        href="{{ route('index') }}#access">アクセス</a>
                 </div>
             </nav>
         </div>
@@ -56,18 +58,20 @@
             </div>
 
             <ul class="text-white text-left hidden md:flex flex-wrap flex-col h-12 md:w-128">
-                <li class="ml-6"><a href="/" class="hover:underline">ホーム</a></li>
-                <li class="ml-6"><a href="#" class="hover:underline">設備</a></li>
+                <li class="ml-6"><a href="{{ route('index') }}" class="hover:underline">ホーム</a></li>
+                <li class="ml-6"><a href="{{ route('facilities') }}" class="hover:underline">設備</a></li>
                 <li class="ml-6"><a href="{{ route('cats.index') }}" class="hover:underline">ねこちゃんたち</a></li>
                 <li class="ml-6"><a href="{{ route('blogs.index') }}" class="hover:underline">ブログ</a></li>
-                <li class="ml-6"><a href="/#access" class="hover:underline">アクセス</a></li>
-                <li class="ml-6"><a href="#" class="hover:underline">よくあるご質問</a></li>
+                <li class="ml-6"><a href="{{ route('index') }}#access" class="hover:underline">アクセス</a></li>
+                <li class="ml-6"><a href="{{ route('faq') }}" class="hover:underline">よくあるご質問</a></li>
                 <li class="ml-6"><a href="{{ route('contact') }}" class="hover:underline">お問い合わせ</a></li>
                 <li class="ml-6"><a href="#" class="hover:underline">プライバシーポリシー</a></li>
             </ul>
         </div>
     </footer>
     <!-- ▲▲▲▲共通フッター▲▲▲▲　-->
+
+    @stack('scripts')
 </body>
 
 </html>
